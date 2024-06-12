dc990788cb2a86fce1ab869a155788688b01ccf3
deus0ne <d.dmitrievhh@gmail.com>
2024-06-12 23:58:30 +0200

    Update cachyos-kernel-override.sh

--- config/scripts/cachyos-kernel-override.sh
+++ config/scripts/cachyos-kernel-override.sh
@@ -11,11 +11,13 @@ setsebool -P domain_kernel_load_modules on
 
 echo 'CachyOS kernel override'
 rpm-ostree cliwrap install-to-root / && \
+curl -Lo /etc/yum.repos.d/_copr_bieszczaders-kernel-cachyos-lto.repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-lto/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-fedora-lto-$(rpm -E %fedora).repo && \
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto  
 
 echo 'Exclude official kernel from updates'
 sed -i '/^\[updates\]/a\exclude\=kernel\*' /etc/yum.repos.d/fedora-updates.repo
 
 echo 'Install uksmd'
+curl -Lo /etc/yum.repos.d/_copr_bieszczaders-kernel-cachyos-addons.repo https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo && \
 rpm-ostree install libcap-ng-devel procps-ng-devel uksmd
 systemctl enable uksmd.service
